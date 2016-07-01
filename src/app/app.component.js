2f9c8a0180b85fafe02373a6b5ae433732bc4825
Melik <mkavcar@gmail.com>
2016-07-01 10:42:44 -0400

    wc

--- src/app/app.component.js
+++ src/app/app.component.js
@@ -5,38 +5,37 @@ angular
     templateUrl: 'app/app.html',
     controller: AppController,
     $routeConfig: [
-      {path: '/myFeed', name: 'MyFeed', component: 'myFeed', useAsDefault: true},
-      {path: '/addSpice', name: 'AddSpice', component: 'addSpice' }
+      {path: '/myFeed', name: 'MyFeed', component: 'myFeed', useAsDefault: true}//,
+      //{path: '/addSpice', name: 'AddSpice', component: 'addSpice' }
     ]
 });
 
-AppController.$inject = ['$rootScope', '$rootRouter', 'authService', 'spiceApi', '$mdDialog'];
+AppController.$inject = ['$rootScope', '$rootRouter', 'authService', 'spiceApi', '$mdDialog', 'searchService', '$scope'];
 
-function AppController($rootScope, $rootRouter, authService, spiceApi, $mdDialog) {
+function AppController($rootScope, $rootRouter, authService, spiceApi, $mdDialog, searchService, $scope) {
   var ctrl = this;
-  ctrl.openAddSpice = openAddSpice;
   $rootScope.auth = authService;
-  $rootScope.activeRoute = 'MyFeed';
+  
+  ctrl.openAddSpice = openAddSpice;
+  ctrl.onSearchChange = onSearchChange;
+
+  searchService.subscribe($scope, function(){
+    ctrl.search = searchService.get().text;
+  });
 
   ////////////
   function openAddSpice(ev) {
     spiceApi.setObj(null);
-    //$rootScope.activeRoute = 'AddSpice';
-    //$rootRouter.navigate(['AddSpice']);
 
     $mdDialog.show({
       parent: angular.element(document.body),
-      controller: AddSpiceController,
       template: '<md-dialog flex="80"><add-spice></add-spice></md-dialog>',
       targetEvent: ev,
       fullscreen: true
     });
-
   }
 
-  //$rootRouter.navigate(['MyFeed']);
-};
-
-function AddSpiceController() {
-
+  function onSearchChange() {
+    searchService.publish(ctrl.search);
+  }
 };

a6372c74a4a66b13e4f4aaeda5278d92676e7e05
Melik <mkavcar@gmail.com>
2016-07-01 10:45:44 -0400

    wc

--- src/app/myfeed/spicecard.component.js
+++ src/app/myfeed/spicecard.component.js
@@ -9,28 +9,38 @@ angular
     templateUrl: 'app/myfeed/spice.html'
 });
 
-SpiceCardController.$inject = ['spiceApi', '$rootRouter', '$rootScope', 'authService'];
+SpiceCardController.$inject = ['spiceApi', '$rootRouter', '$rootScope', 'authService', '$mdDialog', 'searchService'];
 
-function SpiceCardController(spiceApi, $rootRouter, $rootScope, authService) {
+function SpiceCardController(spiceApi, $rootRouter, $rootScope, authService, $mdDialog, searchService) {
   var ctrl = this,
       user = authService.getUser();
 
   ctrl.togglePin = togglePin;
   ctrl.edit = edit;
   ctrl.delete = remove;
+  ctrl.filter = filter;
   
   ////////////
   function togglePin(spiceObj) {
     spiceApi.togglePin(spiceObj);
   }
   
-  function edit(spiceObj) {
+  function edit(spiceObj, ev) {
     spiceApi.setObj(spiceObj);
-    $rootScope.activeRoute = 'AddSpice';
-    $rootRouter.navigate(['AddSpice']);
+    
+    $mdDialog.show({
+      parent: angular.element(document.body),
+      template: '<md-dialog flex="80"><add-spice></add-spice></md-dialog>',
+      targetEvent: ev,
+      fullscreen: true
+    });
   }
   
   function remove(spiceObj) {
     spiceApi.remove(spiceObj);
+  }
+
+  function filter(value) {
+    searchService.publish(value);
   }
 };
